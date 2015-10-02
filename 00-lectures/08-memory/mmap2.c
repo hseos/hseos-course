2c4dc6fad71c16f7e9843c14356fba2b76914c13
Alexander Chernov <blackav@gmail.com>
2015-10-02 20:03:16 +0300

    lecture 8 sample programs added

---
+++ 00-lectures/08-memory/mmap2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    int page = strtoll(argv[2], NULL, 0);
+    int pagesize = getpagesize();
+    printf("page size: %d\n", pagesize);
+    off_t offset = (off_t) page * pagesize;
+
+    //int fd = open(argv[1], O_RDWR, 0);
+    //printf("fd: %d\n", fd);
+    void *mp = mmap(NULL, pagesize, PROT_READ | PROT_WRITE,
+                    MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    if (!~(intptr_t) mp) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return 1;
+    }
+    printf("ptr: %p\n", mp);
+    printf("pid: %d\n", getpid());
+    int *data = mp;
+    while (1) {
+        printf("%d, %d, %d\n", data[0], data[1], data[2]);
+        struct timeval tt;
+        gettimeofday(&tt, NULL);
+        data[0] = getpid();
+        data[1] = tt.tv_sec;
+        data[2] = tt.tv_usec;
+        sleep(1);
+    }
+}
