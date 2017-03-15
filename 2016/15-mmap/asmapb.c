ab25864f5fb40ccf6d4f643973f8365fe24ab63c
Alexander Chernov <blackav@gmail.com>
2017-03-15 14:53:53 +0300

    inline assembly example added

---
+++ 2016/15-mmap/asmapb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    while (scanf("%d%d", &a, &b) == 2) {
+        asm("movl %1, %%eax\n"
+            "movl %2, %%edx\n"
+            "add  %%edx, %%eax\n"
+            "movl %%eax, %0"
+            : "=g" (c)
+            : "g" (a), "g" (b));
+        printf("%d\n", c);
+    }
+}
