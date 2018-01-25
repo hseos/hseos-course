48c52478fbaa46a12de97a941f118e1c82a66de0
Alexander Chernov <blackav@gmail.com>
2018-01-25 16:37:37 +0300

    seminars 09 and 10 added

---
+++ 2017/09-asm2/printflags.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d;
+    while (scanf("%d%d", &a, &b) == 2) {
+        asm("movl   %2, %%eax\n"
+            "addl   %3, %%eax\n"
+            "pushf\n"
+            "popl   %%edx\n"
+            "movl   %%eax, %0\n"
+            "movl   %%edx, %1\n"
+            : "=g" (c), "=g" (d)
+            : "g" (a), "g" (b)
+            : "eax", "edx");
+        printf("%d OF=%d SF=%d ZF=%d CF=%d\n", c, (d >> 11) & 1, (d >> 7) & 1, (d >> 6) & 1, d & 1);
+    }
+}
